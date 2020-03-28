b4e0df80c97db666b180769f1e81ecab08a8e01e
151515243 <62810458+151515243@users.noreply.github.com>
2020-03-29 03:45:18 +0900

    Created using Colaboratory

---
+++ 1.py
@@ -0,0 +1,46 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "1.ipynb",
+      "provenance": [],
+      "collapsed_sections": [],
+      "mount_file_id": "14vUq6SaE0pzExHQBgn7-7V452CoYqHZN",
+      "authorship_tag": "ABX9TyM7MeqmAce/fC9kS15SFiRd",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/151515243/python1/blob/master/1.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "LvQaz-JLEsvU",
+        "colab_type": "code",
+        "colab": {}
+      },
+      "source": [
+        "pricepen=int(input(\"input number of pen: \"))*2000\n",
+        "pricepencil=int(input(\"input number of pencil: \"))*1000\n",
+        "price=pricepen + pricepencil\n",
+        "print(\"Total price is %d.\" %price)"
+      ],
+      "execution_count": 0,
+      "outputs": []
+    }
+  ]
+}
